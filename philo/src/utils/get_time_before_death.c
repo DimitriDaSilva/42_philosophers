5180d62f80c78f4d5b2ee073c9eab7252008cf7a
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2021-07-20 11:11:46 +0100

    Debugged program on mac and fixed issue with 2 60 60 60

--- philo/src/utils/get_time_before_death.c
+++ philo/src/utils/get_time_before_death.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_time_before_death.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dda-silv <dda-silv@student.42lisboa.c      +#+  +:+       +#+        */
+/*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 14:52:23 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/07/07 17:09:09 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/07/20 11:09:26 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,7 @@ long	get_time_before_death(t_simul *simul, struct timeval *last_meal)
 	time_to_die = simul->settings->time_to_die;
 	time_since_last_meal = get_time_since_last_meal(last_meal);
 	time_before_death = time_to_die - time_since_last_meal;
+	if (time_before_death < 0)
+		time_before_death = 0;
 	return (time_before_death);
 }
